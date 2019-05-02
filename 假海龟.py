4dbb374297757b497ed5b565e59650b00eb03c6f
laowu2019 <49662900+laowu2019@users.noreply.github.com>
2019-05-02 19:39:50 +0800

    海龟汉化大部完成

---
+++ 假海龟.py
@@ -0,0 +1,16 @@
+import turtle
+
+海龟 = turtle.Turtle
+
+class Turtle:
+    前进 = turtle.forward
+    后退 = turtle.backward
+    右转 = turtle.right
+    左转 = turtle.left
+    前往 = turtle.goto
+
+前进 = turtle.forward
+后退 = turtle.backward
+右转 = turtle.right
+左转 = turtle.left
+前往 = turtle.goto
---
+++ 常量汉化.py
@@ -0,0 +1,16 @@
+# 常量汉化
+
+空 = None
+红 = 'red'
+黄 = 'yellow'
+蓝 = 'blue'
+黑 = 'black'
+白 = 'white'
+绿 = 'green'
+左 = 'left'
+右 = 'right'
+正常 = 'normal'
+斜体 = 'italic'
+粗体 = 'bold'
+是, 真 = True, True
+否, 假 = False, False
