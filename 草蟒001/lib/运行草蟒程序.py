1201709a86f4f02e9cf1b7bfa6dbafd04d531df2
laowu2019 <49662900+laowu2019@users.noreply.github.com>
2019-06-08 11:02:54 +0800

    无需code runner
    修改vs code 配置文件 launch.json, 按 F5 即可调试, 无需借助code runner.

--- 草蟒001/lib/运行草蟒程序.py
+++ 草蟒001/lib/运行草蟒程序.py
@@ -1,14 +1,16 @@
-# 运行中文程序的入口
+# 运行草蟒程序的入口
 # coding='UTF-8'
 
 from 汉化包 import * 
 import sys
-#import os
+import os
 
+sys.path.append(os.path.dirname(sys.argv[1]))
 #try:
 #os.chdir(os.path.dirname(__file__))
 #print(os.getcwd())
 exec(代码回译(sys.argv[1]))
 #exec(代码回译('示例1.py'))
 #except:
-#    print('没有输入或找到要运行的文件。')
+    #print('没有输入或找到要运行的文件。')
+    #pass
